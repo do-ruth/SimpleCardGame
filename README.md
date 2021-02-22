38d884649b671405dfa035a807eb1e4da36315ff
Ruth Do <ruthiedo99@gmail.com>
2021-02-22 11:27:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Card Game
+================
+
+This is a simple card game where whoever has the highest cards will win.
---
+++ src/cardGame.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class cardGame {
+    public static void main (String[] args){
+
+        Scanner mrScanner = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = "";
+        name = mrScanner.nextLine();
+        System.out.println("Hello " + name);
+
+    }
+
+}
